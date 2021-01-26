7fb4b8765d2f1630846a8701e85632dc76f58220
Yehor Pliasov <fenix4088@gmail.com>
2021-01-26 12:28:31 +0100

    Top menu-click outside of the menu is done

--- src/p2-homeworks/h5/HW5.tsx
+++ src/p2-homeworks/h5/HW5.tsx
@@ -1,4 +1,4 @@
-import React, {MouseEvent, useState} from "react";
+import React, {MouseEvent, useEffect, useState} from "react";
 import Header from "./Header";
 import Routes from "./Routes";
 import { HashRouter } from "react-router-dom";
@@ -8,6 +8,11 @@ import s from "./Styles.module.scss";
 function HW5() {
     const [menuStatus, setMenuStatus] = useState<boolean>(false);
     const toggleMenu = () => setMenuStatus(!menuStatus);
+
+    useEffect(() => {
+        const body = document.querySelector("body");
+        body && body.addEventListener("click", () => setMenuStatus(false));
+    })
 
     return (
     <div className={s.HW5Container}>
