6981fd487b84ac8c2108f12208c488cdaf4113fd
Yehor Pliasov <fenix4088@gmail.com>
2021-02-06 22:00:59 +0100

    HW9 added main logic and Tooltip(alpha version)

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -3,41 +3,63 @@ import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0);
-    const [date, setDate] = useState<Date>();
+    const [date, setDate] = useState<Date>(new Date);
     const [show, setShow] = useState<boolean>(false);
+    const [position, setPosition] = useState<any>({top: 0, left: 0})
 
     const stop = () => {
-        // stop
+        clearTimeout(timerId)
     }
     const start = () => {
         stop();
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date);
         }, 1000);
         setTimerId(id);
     }
 
-    const onMouseEnter = () => {
-        // show
+    const onMouseEnter = (e: any) => {
+        setShow(true);
     };
     const onMouseLeave = () => {
-        // close
+        setShow(false);
+        setPosition({top: 0, left: 0})
     };
 
-    const stringTime = "Time"; // fix with date
-    const stringDate = "Date"; // fix with date
+    const onMouseMove = (e: any) => {
+
+        const {x, y} = getCoordinates(e)
+        setShow(true);
+        setPosition({top: y + 10, left: x + 10})
+    }
+
+    const getCoordinates = (e: any):any => {
+        const {clientY, clientX} = e
+        const {x, y} = e.currentTarget.getBoundingClientRect()
+        return {x: clientX - x, y: clientY - y}
+    }
+
+    const stringTime = date.toLocaleTimeString("ru");
+    const stringDate = date.toLocaleDateString("en", {
+        weekday: 'long',
+        year: "numeric",
+        month: 'long',
+        day: 'numeric'
+    });
 
     return (
-        <div>
+        <div style={{position: 'relative', userSelect: 'none'}}>
             <div
                 onMouseEnter={onMouseEnter}
                 onMouseLeave={onMouseLeave}
+                 onMouseMove={onMouseMove}
             >
                 {stringTime}
             </div>
 
+
             {show && (
-                <div>
+                <div style={{position: "absolute", top: `${position.top}px`, left: `${position.left}px`}}>
                     {stringDate}
                 </div>
             )}
